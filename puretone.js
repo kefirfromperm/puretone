1290ac29196643ec389aeee04e043830013cf16f
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2025-04-20 15:03:14 +0200

    Share buttons logarithmic slider fix bugs

--- puretone.js
+++ puretone.js
@@ -10,19 +10,28 @@ function fixFragment() {
   window.location.hash = '#440'
 }
 
+function updateTitle(newFreq) {
+  $('title').html(`${newFreq} Hz &mdash; Pure Tone Online`);
+}
+
+function updateSlider(newFreq) {
+  $('#freq-slider').val(Math.log10(newFreq / 2));
+}
+
 $(document).ready(function () {
-  const $freqSlider = $('#freq-slider');
   const $freqDisplay = $('#freq-display');
   const $gainSlider = $('#gain-slider');
+
+  // Initial frequency
+  const initialFreq = frequency();
+  updateSlider(initialFreq);
+  $freqDisplay.text(initialFreq);
+  updateTitle(initialFreq);
+
 
   if (!isFragmentValid()) {
     fixFragment();
   }
-
-  // Initial frequency
-  const initialFreq = frequency();
-  $freqSlider.val(initialFreq);
-  $freqDisplay.text(initialFreq);
 
   $('#play-button').click(function () {
     if (!isPlaying()) {
@@ -35,31 +44,71 @@ $(document).ready(function () {
   // Update frequency by hash
   $(window).on("hashchange", function () {
     if (isFragmentValid()) {
-      const newFreq = frequency();
-      $freqSlider.val(newFreq);
+      const newFreq = frequency()
+      updateSlider(newFreq);
       $freqDisplay.text(newFreq);
+      updateTitle(newFreq);
       if (oscillator !== undefined) {
-        oscillator.frequency.setTargetAtTime(newFreq, audioContext.currentTime, 0.01)
+        oscillator.frequency.value = newFreq
       }
     } else {
       fixFragment()
     }
-    play()
   })
 
   // Update hash by slider
-  $freqSlider.on('input', function () {
-    const val = $(this).val();
+  $('#freq-slider').on('input', function () {
+    const val = Math.round(2 * 10 ** $(this).val() * 100) / 100
     window.location.hash = `#${val}`;
     $freqDisplay.text(val);
+    play()
   });
 
   // Volume
   $gainSlider.on('input', function () {
     const val = $(this).val();
     if (gainNode) {
-      gainNode.gain.setTargetAtTime(val / 100, audioContext.currentTime, 0.01);
+      gainNode.gain.value = val / 100
     }
+    play()
+  });
+
+  $('.note-button').click(function () {
+    window.location = $(this).attr('href')
+    play();
+  })
+
+  // Share buttons click handler
+  $('.share-btn').on('click', function () {
+    // Get current URL including the hash part (frequency value)
+    const pageUrl = window.location.href;
+    const pageTitle = document.title;
+    const platform = $(this).data('platform');
+
+    let shareUrl;
+    const windowOptions = 'width=600,height=400,resizable=yes,scrollbars=yes,status=yes';
+
+    switch (platform) {
+      case 'facebook':
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pageUrl)}`;
+        break;
+      case 'twitter':
+        shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(pageUrl)}&text=${encodeURIComponent(pageTitle)}`;
+        break;
+      case 'linkedin':
+        shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(pageUrl)}`;
+        break;
+      case 'telegram':
+        shareUrl = `https://telegram.me/share/url?url=${encodeURIComponent(pageUrl)}&text=${encodeURIComponent(pageTitle)}`;
+        break;
+      case 'whatsapp':
+        shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(pageTitle + ' ' + pageUrl)}`;
+        break;
+      default:
+        return;
+    }
+
+    window.open(shareUrl, 'share', windowOptions);
   });
 })
 
@@ -88,10 +137,8 @@ function play() {
     audioContext.resume()
   }
 
-  if (isPlaying()) {
-    $('#play-button-icon').removeClass('bi-play-fill').addClass('bi-pause-fill')
-    $('#play-button-text').text('Pause')
-  }
+  $('#play-button-icon').removeClass('bi-play-fill').addClass('bi-pause-fill')
+  $('#play-button-text').text('Pause')
 }
 
 function pause() {
